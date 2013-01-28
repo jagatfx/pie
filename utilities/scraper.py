7fe08a81b6874d01ca77467bdb49293acef1b8b3
Marvin Zhang <zhangmarvin95@gmail.com>
2013-01-28 02:56:09 -0800

    scraper images

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -7,25 +7,20 @@ OUTPUT_FILE = 'data.sql'
 def store(output, full_name, party, constit, twitter, image):
     preamble = "INSERT INTO website_mp (name, party, constituency, twitter_handle) VALUES ("
     full_name = full_name.strip()
-    image_name = full_name.replace(' ', '_')
-    #    with open(image_name, 'w') as dest: # save the image
-    #        print(image)
-    #        print(full_name)
-    #        try:
-    #            img = urllib2.urlopen(image + '.jpg')
-    #        except urllib2.HTTPError:
-    #            try:
-    #                img = urllib2.urlopen(image + '.jpeg')
-    #            except urllib2.HTTPError:
-    #                try:
-    #                    img = urllib2.urlopen(image + '.png')
-    #                except urllib2.HTTPError:
-    #                    try:
-    #                        img = urllib2.urlopen(image + '.JPG')
-    #                    except urllib2.HTTPError:
-    #                        img = urllib2.urlopen(image)
-    #   img = img.read()
-    #   dest.write(img)
+    if image[-4] == '.':
+        ext = image[-4:]
+    else:
+        ext = image[-5:]
+    image_name = full_name.lower().replace(' ', '_') + ext
+    with open(image_name, 'w') as dest: # save the image
+        print(image)
+        print(full_name)
+        try:
+            img = urllib2.urlopen(image)
+            img = img.read()
+            dest.write(img)
+        except urllib2.HTTPError:
+            print('***{0}***'.format(image)) 
     l = [full_name, party, constit, twitter]
     l = list(map(lambda x: "'" + x + "'", l))
     output.write(preamble + ', '.join(l) + ")\n")
@@ -41,7 +36,8 @@ with open(OUTPUT_FILE, 'w') as f:
         for politician in source:
             constit = politician.split('h3')[1][1:-2]
             image = politician.split('src="')[1]
-            image = image.split('_normal')[0]
+            image = image.split('">\n\t\t')[0]
+            image.replace('_normal', '')
             politician = politician.split('tweetersModuleCopyLeftside')[2]
             politician = politician.split('\n\t\t\t\t')
             twitter = politician[2].split('\t\t\t')[0]
