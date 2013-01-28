db652102259f6ef056d9ff0fa0db16683a8080aa
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 03:22:53 -0800

    fixed dependencies

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from website.models import MP
 
+import os
 import oauth2, urllib, urllib2, json
 
 def home(request):
