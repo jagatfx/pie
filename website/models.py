7e56962035fc9433bd91f4ab581a2101b155776a
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 02:19:52 -0800

    names are dumb

--- website/models.py
+++ website/models.py
@@ -6,8 +6,7 @@ class MP(models.Model):
     class Meta:
         verbose_name = "MP"
         verbose_name_plural = "MP's"
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     party = models.CharField(max_length=30) 
     constituency = models.CharField(max_length=30)
     twitter_handle = models.CharField(max_length=30)
