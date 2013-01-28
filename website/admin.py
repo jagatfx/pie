e2ddbef8f823489d0a49ebe75b64b569aa5ff2d0
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 01:56:53 -0800

    customizations for admin

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,12 @@
 from website.models import MP, Term, TermAlias
 from django.contrib import admin
 
-admin.site.register(MP)
-admin.site.register(Term)
+class MPAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'party', 'constituency', 'twitter_handle']
+    list_filter = ['party']
+admin.site.register(MP, MPAdmin)
+
+class TermAdmin(admin.ModelAdmin):
+    list_display = ['title']
+admin.site.register(Term, TermAdmin)
 admin.site.register(TermAlias)
--- website/models.py
+++ website/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class MP(models.Model):
+    class Meta:
+        verbose_name = "MP"
+        verbose_name_plural = "MP's"
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     party = models.CharField(max_length=30) 
@@ -10,9 +13,18 @@ class MP(models.Model):
     twitter_handle = models.CharField(max_length=30)
 
 class Term(models.Model):
+    class Meta:
+        verbose_name = "Term"
+        verbose_name_plural = "Terms"
     title = models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return self.title
+
 class TermAlias(models.Model):
+    class Meta:
+        verbose_name = "Term Alias"
+        verbose_name_plural = "Term Aliases"
     term = models.ForeignKey('Term')
     alias = models.CharField(max_length=30)
 
