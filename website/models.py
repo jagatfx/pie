9f06d0d38110289a327f8418d7a2116312d49f67
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 00:00:18 -0800

    added database schema

--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class MP(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    party = models.CharField(max_length=30) 
+    constituency = models.CharField(max_length=30)
+    twitter_handle = models.CharField(max_length=30)
+
+class Term(models.Model):
+    title = models.CharField(max_length=30)
+
+class TermAliase(models.Model):
+    term = models.ForeignKey('Term')
+    alias = models.CharField(max_length=30)
+
