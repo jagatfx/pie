28c977f5c3103d660852c60a2f00e8ecf1b43493
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 03:20:26 -0800

    added mp view

--- website/views.py
+++ website/views.py
@@ -1,5 +1,14 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from website.models import MP
 
 def home(request):
-    return render_to_response("base.html", RequestContext(request))
+    return render_to_response('base.html', RequestContext(request))
+
+def mp_detail(request, mp_name):
+    mp = MP.objects.get(name=mp_name.replace('_', ' '))
+    data = {"name": mp.name,
+            "party": mp.party,
+            "constituency": mp.constituency,
+            "twitter": mp.twitter_handle}
+    return render_to_response('mp.html', RequestContext(request, data))
