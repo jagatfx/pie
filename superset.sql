b10268ef556da65569dfdd0a801933d88c6649c4
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-28 02:26:45 -0800

    sqlified superset

--- superset.sql
+++ superset.sql
@@ -1,22 +1,70 @@
-Finance
-finance, financial, tax, pay, money, banks, regulatory, licensing, pensions, fund, economy, budget, austerity
-Legal
-legal, punishment, offenders, prosecutions, police, social, crime, criminals, firearms
-Jobs
-jobs, employment, growth
-Welfare
-welfare, health, NHS, housing, planning, union
-Foreign
-foreign, european, commonwealth, euro, infrastructure, referendum, immigration
-Defence
-defence, armed forces, military
-Education
-education, schools
-Energy
-energy, rails
-Badgers
-badgers
-Scottish Independence
-scottish independence, scotland
-Heathrow Runway 4
-heathrow runway 4
+-- 'Finance'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'finance')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'financial')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'tax')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'pay')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'money')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'bank')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'regulatory')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'licensing')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'pension')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'fund')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'economy')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'budget')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Finance' , 'austerity')
+
+-- 'Legal'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'legal')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'punishment')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'offender')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'prosecution')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'police')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'social')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'crime')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'criminal')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Legal' , 'firearm')
+
+-- 'Jobs'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Jobs' , 'jobs')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Jobs' , 'employment')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Jobs' , 'growth')
+
+-- 'Welfare'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'welfare')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'health')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'NHS')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'housing')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'planning')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Welfare' , 'union')
+
+-- 'Foreign'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'foreign')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'european')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'commonwealth')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'euro')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'infrastructure')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'referendum')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Foreign' , 'immigration')
+
+-- 'Defence'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Defence' , 'defence')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Defence' , 'armed forces')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Defence' , 'military')
+
+-- 'Education'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Education' , 'education')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Education' , 'school')
+
+-- 'Energy'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Energy' , 'energy')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Energy' , 'rail')
+
+-- 'Badgers'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Badgers' , 'badgers')
+
+-- 'Scottish Independence'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Scottish Independence' , 'scottish independence')
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Scottish Independence' , 'scotland')
+
+-- 'Heathrow Runway 4'
+INSERT INTO website_termalias (term_id, alias) VALUES ((SELECT id FROM website_term WHERE title ='Heathrow Runway 4' , 'heathrow runway 4')
