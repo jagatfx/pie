46ce28ced17a40653238b23c0cbccfd92a059a54
Marvin Zhang <zhangmarvin95@gmail.com>
2013-01-28 00:46:31 -0800

    superset

---
+++ superset.sql
@@ -0,0 +1,22 @@
+Finance
+finance, financial, tax, pay, money, banks, regulatory, licensing, pensions, fund, economy, budget, austerity
+Legal
+legal, punishment, offenders, prosecutions, police, social, crime, criminals, firearms
+Jobs
+jobs, employment, growth
+Welfare
+welfare, health, NHS, housing, planning, union
+Foreign
+foreign, european, commonwealth, euro, infrastructure, referendum, immigration
+Defence
+defence, armed forces, military
+Education
+education, schools
+Energy
+energy, rails
+Badgers
+badgers
+Scottish Independence
+scottish independence, scotland
+Heathrow Runway 4
+heathrow runway 4
