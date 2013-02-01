f190fc6b28bbfe3f7cfc672f1ddb8de634ee5c48
Shreyas Chand <shreyas.chand@gmail.com>
2013-02-01 02:27:52 -0800

    added empty about page

--- pie/urls.py
+++ pie/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^$', 'website.views.home'),
     url(r'^mp/$', 'website.views.mp_overview'),
     url(r'^mp/(?P<mp_name>\w+)/$', 'website.views.mp_detail'),
+    url(r'^about/$', direct_to_template, {'template': 'about.html'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
