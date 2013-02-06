c91713da0a073d1c2c300a44433c3e1e776a9e43
Shreyas Chand <shreyas.chand@gmail.com>
2013-02-06 00:37:38 -0800

    Removed Brian's config. (You dummy)

--- pie/settings.py
+++ pie/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(default="postgres://brian:hello@localhost:5432/pie")
+DATABASES['default'] = dj_database_url.config(default="postgres://localhost:5432/pie")
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
