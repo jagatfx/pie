53054a947280f23e8af2bcd06ec02d198e83fb3d
Shreyas Chand <shreyas.chand@gmail.com>
2013-03-12 20:56:49 -0700

    added live tweet feed

--- website/static/js/application.js
+++ website/static/js/application.js
@@ -1,4 +1,11 @@
 function fetchTweets() {
-    // tweet fetching code
-    //alert();
+    $.get("/ajax/feed/", function(data) {
+        var tweets = "";
+        $.each(data, function(key, value) {
+             tweets += "<p><i class=\"icon-chevron-right\"></i>" + value.from
+                      + ": " + value.content + " <span class=\"muted\"><i>- "
+                      + value.date + "</i></span></p>";
+        });
+        $("#tweetWall").html(tweets);
+    });
 }
