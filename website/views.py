1d2537b585646e1e7bad81be82128f373ffe6d70
Shreyas Chand <shreyas.chand@gmail.com>
2013-04-23 12:48:17 -0700

    finished up setting up the media pages

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 # Imports
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from website.models import Politico
+from website.models import Politico, Media
 from website.twitter import *
 from django.http import HttpResponse
 import json
@@ -62,3 +62,29 @@ def lord_detail(request, mp_name):
 
 def live_tweet_feed(request):
     return HttpResponse(json.dumps(tweets_all()), content_type="application/json")
+
+def media_overview(request):
+    meds = []
+    media_list = Media.objects.all().order_by('name')
+
+    for media in media_list:
+        media_desc = {"name": media.name,
+                      "url": media.name.replace(" ", "_"),
+                      "party_leaning": media.party_leaning,
+                      "affiliation": media.affiliation}
+        meds.append(media_desc)
+    data = {"media": True, "medias": meds, "title": "All Media Personalities"}
+    return render_to_response('media_overview.html', RequestContext(request, data))
+
+def media_detail(request, media_name):
+    media = Media.objects.get(name=media_name.replace('_', ' '))
+    data = {"politico": True,
+            "name": media.name,
+            "image_url": "http://pie.shreyaschand.com/img/media/" + media_name + ".png",
+            "party_leaning": media.party_leaning,
+            "affiliation": media.affiliation,
+            "twitter_handle": media.twitter_handle,
+            "tweets_by_them": tweets_by(media.twitter_handle),
+            "tweets_at_them": tweets_at(media.twitter_handle)
+            }
+    return render_to_response('media_detail.html', RequestContext(request, data))
