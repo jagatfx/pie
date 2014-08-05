2b4cbb5c1c07ac43d372647b428b941543587ff4
Jacob Madden <jacob.madden@connectsolutions.com>
2014-08-04 21:44:37 -0400

    attempt to fix encoding error by setting default

--- pie/wsgi.py
+++ pie/wsgi.py
@@ -14,9 +14,7 @@ framework.
 
 """
 import os
-import sys
 
-sys.setdefaultencoding("utf-8")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pie.settings")
 
 # This application object is used by any WSGI server configured to use this
